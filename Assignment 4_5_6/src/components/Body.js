92262e3e0e0a4996dee3fb53ec8e19a7e1e6e28e
av1nDs0uza <avinalricdsouza@gmail.com>
2024-12-05 23:39:23 +0530

    assginment 6

--- Assignment 4_5_6/src/components/Body.js
+++ Assignment 4_5_6/src/components/Body.js
@@ -1,11 +1,32 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockdata";
-import { useState } from "react";
+import Shimmer from "./Shimmer";
+import { useEffect, useState } from "react";
+
 
 
 
 const Body = () => {
-    const [listOfRestaurants, setListOfRestaurant] = useState(resList);
+    const [listOfRestaurants, setListOfRestaurant] = useState([]);
+
+    useEffect(() =>{
+        fetchData();
+    },[]); 
+
+    const fetchData = async () => {
+        const data = await fetch(
+          "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        ); 
+    
+        const json = await data.json();
+    
+        console.log(json);
+        //Optional Chaining
+        setListOfRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      };
+    if(listOfRestaurants.length == 0) {
+        return <Shimmer/>;
+      };
     return(
         <div className="body">
             <div className="search">
--- Assignment 4_5_6/src/components/Header.js
+++ Assignment 4_5_6/src/components/Header.js
@@ -1,6 +1,9 @@
 import { LOGO_URL } from "../utils/constant";
+import { useState } from "react";
 const Header = () => {
+    const [btnNameReact,setBtnNameReact] = useState("Login"); 
     return(
+
         <div className="header">
             <div>
                 <img className="logo"  alt="restaurant logo" src={LOGO_URL} />
@@ -11,6 +14,11 @@ const Header = () => {
                 <li>About Us</li>
                 <li>Contact Us</li>
                 <li>🛒</li>
+                <button className="login" onClick={() =>{
+                    btnNameReact == "Login"? setBtnNameReact("Logout") : setBtnNameReact("Login") ;
+                }}>
+                {btnNameReact}
+                </button>
                 </ul>
             </div>
 
